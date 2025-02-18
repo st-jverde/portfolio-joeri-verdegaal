5c9ed5623264901f00d207bdf42742535227c467
Joeri Verdegaal <joeriverdegaal@gmail.com>
2025-02-18 12:16:54 +0100

    basic design, install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Adjust paths as needed
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
